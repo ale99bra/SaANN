2a3aabb75b8a9da9abc00bc4defa04d13a53d1eb
Alessio <alessio.branda@outlook.com>
2026-06-05 12:05:38 +0200

    Implemented AdamW

--- saann/gradients.py
+++ saann/gradients.py
@@ -48,3 +48,53 @@ class SGD:
         else:
             import warnings
             warnings.warn("Gradients not found for a layer. Skipping update.")
+
+class AdamW:
+
+    def __init__(self, params, learning_rate = 1e-3, beta1 = 0.9, beta2 = 0.999, wd = 0.0, eps = 1e-8):
+        self.params = params
+        self.lr = learning_rate
+        self.B1 = beta1
+        self.B2 = beta2
+        self.eps = eps
+        self.wd = wd
+        
+        #initialize moments
+        self.m = {}
+        self.v = {}
+
+        for name, p in self.params.items():
+            if name.startswith("d"): continue
+            self.m[name] = BE.xp.zeros_like(p)
+            self.v[name] = BE.xp.zeros_like(p)
+
+        self.t = 0
+
+    def step(self):
+
+        self.t += 1
+
+        for name, g in self.params.items():
+            if not name.startswith("d"): continue
+            param_name = name[1:]
+            param = self.params[param_name]
+
+            if self.wd > 0.0:
+                param -= self.lr * self.wd * param
+
+            #update bias for m
+            self.m[param_name] = (self.B1 * self.m[param_name] + (1-self.B1) * g)
+
+            #update bias for v
+            self.v[param_name] = (self.B2 * self.v[param_name] + (1-self.B2) * g**2)
+
+            #correct biases
+            m_hat = self.m[param_name] / (1-self.B1**self.t)
+            v_hat = self.v[param_name] / (1-self.B2**self.t)
+
+            #update param
+            param -= self.lr * m_hat/(BE.xp.sqrt(v_hat)+self.eps)
+    
+    def zero_grad(self):
+        for name, p in self.params.items():
+            if name.startswith("d"): p[...]=0
